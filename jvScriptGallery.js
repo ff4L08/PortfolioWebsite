4f64068b7c671a42f4de1b7cb2338c689652bab2
Andy Wang <awang21108@gmail.com>
2025-06-28 11:30:40 +0700

    upload code

---
+++ jvScriptGallery.js
@@ -0,0 +1,14 @@
+function hideTable() {
+    // hides the gallery
+    document.getElementById('gallery').style.display = 'none';
+    
+}
+
+function ReplaceImage() {
+    // hides the image
+    document.getElementById('mainImage').style.display = 'none';
+    //show the grid
+    document.getElementById('gallery').style.display = 'grid';
+}
+
+hideTable()
