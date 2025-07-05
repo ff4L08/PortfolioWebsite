09ba887d903591d60497df7090f073364a4de141
Andy Wang <awang21108@gmail.com>
2025-07-05 11:01:56 +0700

    update file

---
+++ Javascript/Gallery.js
@@ -0,0 +1,11 @@
+var gallery = document.getElementById('gallery')
+
+var img = document.createElement("img");
+img.src = "images/1.png";
+gallery.appendChild(img);
+
+
+
+// for (let i = 0; i < cars.length; i++) {
+//     text += cars[i] + "<br>";
+//   } 
---
+++ Javascript/GalleryShow.js
@@ -0,0 +1,28 @@
+function hideTable() {
+    // hides the gallery
+    document.getElementById('gallery').style.display = 'none';
+    document.getElementById('ChangeBack').style.display = 'none';
+
+    
+}
+
+function ReplaceImage() {
+    // hides the image
+    document.getElementById('mainImage').style.display = 'none';
+    //show the grid
+    document.getElementById('gallery').style.display = 'grid';
+    document.getElementById('ChangeBack').style.display = 'block';
+    document.getElementById('ShowGallery').style.display = 'none';
+
+}
+
+function ReplaceGallery() {
+    // hides the image
+    document.getElementById('mainImage').style.display = 'initial';
+    //show the grid
+    document.getElementById('gallery').style.display = 'none';
+    document.getElementById('ChangeBack').style.display = 'none';
+    document.getElementById('ShowGallery').style.display = 'block';
+}
+
+hideTable()
