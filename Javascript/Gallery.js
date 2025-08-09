87f1332221414f98131d2a20e57cedef474826ed
Andy Wang <awang21108@gmail.com>
2025-08-09 09:42:09 +0700

    update

--- Javascript/Gallery.js
+++ Javascript/Gallery.js
@@ -1,11 +1,42 @@
-var gallery = document.getElementById('gallery')
+const galleryElement = document.getElementById('gallery')
+const imagesDir = "images/GalleryImages/"
+var imageNames = ["1.png", "2.png"]; 
 
-var img = document.createElement("img");
-img.src = "images/1.png";
-gallery.appendChild(img);
+function GetImageName() {
+
+}
+
+function loadImages(imageNames) {
+    
+    console.log("start")
+
+    imageNames.forEach(element => {
+        try{
+            const imgElement = document.createElement('img');
+            var imageDir = imagesDir + element
+            imgElement.src = imageDir;
+            console.log("image directory: " + imageDir)
+            galleryElement.style.width = "100 px";
+            galleryElement.appendChild(imgElement);
+        } catch (error) {
+            console.error("Could not fetch or display images:", error);
+            // Display a message to the user if images fail to load
+            galleryElement.innerHTML = '<p>Failed to load images. Please try again later.</p>';
+        }
+    });
+}
+
+loadImages(imageNames)
+
+// imageAvailable = true
+// imageName = 0
+// while (imageAvailable) {
 
 
+//     // imageName += 1;
+//     // directoryName = "images/GalleryImages/" + String(imageName) + "png";
 
-// for (let i = 0; i < cars.length; i++) {
-//     text += cars[i] + "<br>";
-//   } 
+//     // var img = document.createElement("img");
+//     // img.src = directoryName;
+//     // gallery.appendChild(img)
+// }
